a3832de117a8c5e7844032194a9d9dbfc48328c5
ANUHAS56 <156334107+ANUHAS56@users.noreply.github.com>
2024-11-01 01:08:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=i6xDgI5L#gmTG9B050siUJDxH3IyUZxueo6by1OxkvhzT3QundWw",
+MONGODB: process.env.MONGODB || "mongodb+srv://nadu:09hxVnNovnrgwiPr@cluster0.y30bp.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
